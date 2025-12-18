9362284f79bfa3bdd0e6587fc9a3d79a55b25b97
Karina <klochankakarina@gmail.com>
2025-12-18 21:06:38 +0100

    Check if direcctories exist or are accessiable

--- filemover.sh
+++ filemover.sh
@@ -9,8 +9,14 @@ echo "Thank you. File extension: $file_extension"
 # Запросить новое расширение для файлов.
 
 # Проверка существования исходной директории и целевой директории
-
-
+for DIR in "$source_directory" "$target_directory" 
+ do
+    if [[ ! -d "$DIR" || ! -r "$DIR" || ! -x "$DIR" ]] 
+      then
+        echo "Error: directory doesn't exist or is inaccessible: $DIR"
+        exit 1
+    fi
+done
 # Проверка, есть ли файлы с указанным расширением в исходной директории
 
 # Копирование файлов с указанным расширением в целевую директорию
